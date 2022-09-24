78d644d7b5d8babb6b8de45f50d91447de851bbe
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-09-24 22:17:49 +0700

    Add file events handler

--- app/index.js
+++ app/index.js
@@ -25,15 +25,59 @@ app.on("ready", () => {
 });
 
 //quit app when all window closed
-app.on('window-all-closed', () => {
-    if( process.platform !== 'darwin' ) {
-        app.quit();
-    }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 // when app activates, open a window
-app.on( 'activate', () => {
-    if( BrowserWindow.getAllWindows().length === 0 ) {
-        openWindow();
-    }
-} );
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    openWindow();
+  }
+});
+
+//get all files on app directory
+ipcMain.handle("app:get-files", () => {
+  return io.getFile();
+});
+
+//add files by drag and drop
+ipcMain.handle("app:on-file-add", (event, files = []) => {
+  io.addFiles(files);
+});
+
+//add files by choosing from dialog
+ipcMain.handle("app:on-fs-dialog-open", (event) => {
+  const files = dialog.showOpenDialogSync({
+    properties: ["openFile", "multiSelections"],
+  });
+
+  io.addFiles(
+    files.map((filepath) => {
+      return {
+        name: path.parse(filepath).base,
+        path: filepath,
+      };
+    })
+  );
+});
+
+//delete file
+ipcMain.on("app:on-file-delete", (event, file) => {
+  io.deleteFile(file.filepath);
+});
+
+//open file
+ipcMain.on("app:on-file-open", (event, file) => {
+  io.openFile(file.filepath);
+});
+
+//copy file
+ipcMain.on("app:on-file-copy", (event, file) => {
+  event.sender.startDrag({
+    file: file.filepath,
+    icon: path.resolve(__dirname, "../resources/paper.png"),
+  });
+});
